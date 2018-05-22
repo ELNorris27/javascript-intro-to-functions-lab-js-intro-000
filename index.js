aa7b5d2b22148f257136104531f6170e17027029
Elizabeth Norris <elnorris27@gmail.com>
2018-05-22 03:21:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,18 +14,18 @@ function logWhisper(string) {
   console.log("Hello".toLowerCase());
 }
 
-+function sayHiToGrandma(string){
-+  var lowercase = string.toLowerCase()
-+  var uppercase = string.toUpperCase()
-+
-+  if(string === lowercase){
-+    return 'I can\'t hear you!'
-+  } else if(string === uppercase) {
-+    return "YES INDEED!"
-+  } if(string === "I love you, Grandma."){
-+    return "I love you, too."
-+  }
-+};
+function sayHiToGrandma(string){
+  var lowercase = string.toLowerCase()
+  var uppercase = string.toUpperCase()
+
+  if(string === lowercase){
+    return 'I can\'t hear you!'
+  } else if(string === uppercase) {
+    return "YES INDEED!"
+  } if(string === "I love you, Grandma."){
+    return "I love you, too."
+  }
+};
 
 /*function sayHiToGrandma(string) {
   var hiGrandma = "Hi Grandma".toLowerCase;
