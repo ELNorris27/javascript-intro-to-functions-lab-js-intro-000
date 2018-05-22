28378ffbb5e8f51c61093e87439770617c530fdf
Elizabeth Norris <elnorris27@gmail.com>
2018-05-22 02:34:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ function shout(string) {
   return string.toUpperCase()
 }
 
-function whisper(string){
+function whisper(string) {
   return 'Hello'.toLowerCase();
+}
+
+function logShout(string) {
+  console.log(string.toUpperCase);
 }
