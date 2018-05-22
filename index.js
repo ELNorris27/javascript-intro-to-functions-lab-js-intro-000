6614f37252d28676b4a00315a669cc80b0ee9600
Elizabeth Norris <elnorris27@gmail.com>
2018-05-22 03:20:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,20 @@ function logWhisper(string) {
   console.log("Hello".toLowerCase());
 }
 
-function sayHiToGrandma(string) {
++function sayHiToGrandma(string){
++  var lowercase = string.toLowerCase()
++  var uppercase = string.toUpperCase()
++
++  if(string === lowercase){
++    return 'I can\'t hear you!'
++  } else if(string === uppercase) {
++    return "YES INDEED!"
++  } if(string === "I love you, Grandma."){
++    return "I love you, too."
++  }
++};
+
+/*function sayHiToGrandma(string) {
   var hiGrandma = "Hi Grandma".toLowerCase;
   var helloGrandma = "Hi Grandma".toUpperCase;
   var loveGranny = "I love you. Grandma."
@@ -27,7 +40,7 @@ function sayHiToGrandma(string) {
    }
 };
 
-/*function sayHiToGrandma(string) {
+function sayHiToGrandma(string) {
   var helloGrandma = "HI, GRANDMA";
   if (helloGrandma.toUpperCase() === helloGrandma) {
     return "YES INDEED!";
